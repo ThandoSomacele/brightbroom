74a0171cba14fcf5f71d4bf35906e37e9f215523
Thando Somacele <thando.somacele@gmail.com>
2025-03-25 18:31:27 +0200

    Remove deprecated files and clean up unused code ✨✨✨

--- +layout.server.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/+layout.server.ts
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-  // Return user data if logged in
-  return {
-    user: locals.user
-  };
-};
