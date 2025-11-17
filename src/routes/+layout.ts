68a33dec207b8b61e8071d250926c8686bbe407e
Thando Somacele <thando.somacele@gmail.com>
2025-11-17 14:17:55 +0200

    feat: Update PostHog integration to use hardcoded API host path

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,11 @@
 import posthog from "posthog-js";
 import { browser } from "$app/environment";
-import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_UI_HOST } from "$env/static/public";
+import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_UI_HOST } from "$env/static/public";
 
 export const load = async () => {
   if (browser) {
     posthog.init(PUBLIC_POSTHOG_KEY, {
-      api_host: PUBLIC_POSTHOG_HOST,
+      api_host: "/relay-VjWm", // Hardcoded relative path - works with any staging/production domain
       ui_host: PUBLIC_POSTHOG_UI_HOST,
       capture_pageview: false,
       capture_pageleave: false,
